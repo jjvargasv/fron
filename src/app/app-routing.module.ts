bf50574080c9271a9ec00a3c536775408553d4aa
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-17 10:40:49 -0500

    [+] app-routing.module: Define rutas a los modulos usando carga perezosa [+] app.component: Implementa <router-outlet> principal [+] (auth/protected)-routing.module: Define rutas de cada modulo [+] main.component: Implementa <router-outlet> para modulo auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /** Define la ruta de los dos modulos usando carga perezosa */
+  {
+    path: 'auth',
+    loadChildren: () => import( './auth/auth.module' )
+      .then( module => module.AuthModule )
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import( './protected/protected.module' )
+      .then( module => module.ProtectedModule )
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
