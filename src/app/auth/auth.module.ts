538b845a4a0e4556f50da32e8a3b53fd5dc26762
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-17 11:23:52 -0500

    [+] auth.module: Importa ReactiveFormsModule para el modulo auth [+] login.component: Implementa validacion de campos usando Reactive-Forms

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { MainComponent } from './pages/main/main.component';
@@ -15,7 +16,8 @@ import { MainComponent } from './pages/main/main.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  // Procuramos usar los mismos nombres que espera nuestra API en las propiedades que agrupamos en nuestro FormBuilder Group
+  loginForm: FormGroup = this.fb.group({
+    email: [
+      '',   // Valor por defecto vacio
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '', // Valor por defecto vacio
+      [
+        Validators.required,
+        Validators.minLength( 6 )
+      ]
+    ]
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  login() {
+    console.group( 'loginForm' );
+    console.log( this.loginForm.value );
+    console.log( this.loginForm.valid );
+    console.groupEnd();
+  }
 
 }
