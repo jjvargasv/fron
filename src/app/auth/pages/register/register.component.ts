0723673de9be5e42c9b684d46cb45cbb4f229747
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-17 11:39:54 -0500

    [+] register.component: Implementa validacion de campos usando Reactive-Forms

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  // Procuramos usar los mismos nombres que espera nuestra API en las propiedades que agrupamos en nuestro FormBuilder Group
+  registerForm: FormGroup = this.fb.group({
+    name: [
+      '',   // Valor por defecto
+      [
+        Validators.required
+      ]
+    ],
+     email: [
+      '',   // Valor por defecto vacio
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '', // Valor por defecto vacio
+      [
+        Validators.required,
+        Validators.minLength( 6 )
+      ]
+    ]
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  register() {
+    console.group( 'registerForm' );
+    console.log( this.registerForm.value );
+    console.log( this.registerForm.valid );
+    console.groupEnd();
+  }
 
 }
