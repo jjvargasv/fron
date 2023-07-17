5775ae19f4b47435e66bfe359f90d69f0ad92667
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-17 12:58:51 -0500

    [+] environment.*: Config variables de entorno [+] auth:.service: Request (POST) /auth/login [+] login.component: Subscribe authService para obtener datos del Observable

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -29,7 +30,8 @@ export class LoginComponent {
 
   constructor(
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   login() {
@@ -38,8 +40,15 @@ export class LoginComponent {
     console.log( this.loginForm.valid );
     console.groupEnd();
 
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login( email, password )
+      .subscribe( response => {
+        console.log( response );
+      });
+
     // this.router.navigate([ 'dashboard' ]);
-    this.router.navigateByUrl( '/dashboard' );
+    // this.router.navigateByUrl( '/dashboard' );
   }
 
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseUrl: string = environment.baseUrl;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login( email: string, password: string ) {
+    const
+      URL = `${ this.baseUrl }/auth/login`,
+      body = {
+        email, password
+      };
+
+    return this.http.post( URL, body );   // Retorna un Observable
+  }
+
 }
