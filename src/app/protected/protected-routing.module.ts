cafc44d3e82e1b47d7863edc0dd959c9debc2a3a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-21 09:48:40 -0500

    [+] protected-routing.module: Agrega ruta /products a modulo 'protected'

--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { validateTokenGuard } from '../auth/guards/validate-token.guard';
 import { MainComponent } from './pages/main/main.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
       {
         path: '',
         component: DashboardComponent
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
       },
       {
         path: '**',
