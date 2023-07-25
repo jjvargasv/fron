96b70c3574a5f6bb877ff4679b47baba437b43f2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-25 11:11:27 -0500

    [+] categories.component: Captura y valida valores de los campos del formulario usando 'Reactive Forms'

--- src/app/protected/pages/categories/categories.component.ts
+++ src/app/protected/pages/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categories',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class CategoriesComponent {
 
+  // Procuramos usar los mismos nombres que espera nuestra API en las propiedades que agrupamos en nuestro FormBuilder Group
+  categoryForm: FormGroup = this.fb.group({
+    name: [
+      '',   // Valor por defecto
+      [
+        Validators.required,
+        Validators.minLength( 3 )
+      ]
+    ],
+    description: [
+      '',  // Valor por defecto
+      []
+    ]
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+
+  createCategory() {
+    console.group( 'categoryForm' );
+    console.log( this.categoryForm.value );
+    console.log( this.categoryForm.valid );
+    console.groupEnd();
+  }
 }
