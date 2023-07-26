7bac92673185c614d08dea9d9000dc65246b49bb
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-26 12:08:35 -0500

    [+] ng g c components/partials/header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/partials/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
