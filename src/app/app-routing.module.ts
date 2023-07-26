5e450658cc69106e7e39873b3da559e3ff106186
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-26 11:06:34 -0500

    [+] ng g c pages/home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   /** Define la ruta de los dos modulos usando carga perezosa */
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'auth',
     loadChildren: () => import( './auth/auth.module' )
