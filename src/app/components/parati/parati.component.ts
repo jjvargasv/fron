f89dc60296e039fbf1a322bbff90a5a1a1c5c830
Jhon Jairo Vargas Vargas <vargas.jhon@gmail.com>
2023-08-17 23:47:30 -0500

    [+] se crorrigen las cars y se los colores de la pagina

--- src/app/components/parati/parati.component.ts
+++ src/app/components/parati/parati.component.ts
@@ -9,11 +9,39 @@ import { ProductsService } from 'src/app/services/products.service';
 })
 export class ParatiComponent  implements OnInit {
   products: Products = { products: [] };
+  cart!: any []
   constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
     this.productsService.getProductByCategory('parati').subscribe((res) => {
       this.products.products = res;
     });
+  }
+  addToCart(product: any) {
+    console.log( product );
+
+    const cartStorage = localStorage.getItem('cart')? JSON.parse(localStorage.getItem('cart')!) : [];
+    this.cart = cartStorage 
+    const foundProduct = this.cart.find(( item )=>{
+      return item._id == product._id
+    })
+    if (foundProduct){
+      foundProduct.quantity = foundProduct.quantity + 1 //foundProduct.quantity += 1
+      foundProduct.total = foundProduct.price*foundProduct.quantity
+      }else{
+        this.cart.push({
+          _id: product._id,
+          name: product.name,
+          price: product.price,
+          quantity: 1,
+          describe: product.description,
+          urlImage: product.urlImage,
+          total: product.price,
+          stock: product.quantity
+
+        })
+
+    }
+  localStorage.setItem('cart', JSON.stringify(this.cart)) 
   }
 }
--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Products } from 'src/app/auth/interfaces/products.interface';
+import { ProductsService } from 'src/app/services/products.service';
 import { ServiciosService } from 'src/app/services/servicios.service';
 
 @Component({
@@ -7,13 +9,39 @@ import { ServiciosService } from 'src/app/services/servicios.service';
   styleUrls: ['./servicios.component.css']
 })
 export class ServiciosComponent implements OnInit{
-  servicios!: any;
-  constructor(private servicio: ServiciosService) { }
+  products: Products = { products: [] };
+  cart!: any[];
+  constructor(private productsService: ProductsService) { }
   ngOnInit(): void {
-    this.servicio.getAllServicios().subscribe(res => {
-      this.servicios = res;
-      console.log(res);
-    })
+    this.productsService.getProductByCategory('servicios').subscribe((res) => {
+      this.products.products = res;
+    });
   }
+  addToCart(product: any) {
+    console.log( product );
 
+    const cartStorage = localStorage.getItem('cart')? JSON.parse(localStorage.getItem('cart')!) : [];
+    this.cart = cartStorage 
+    const foundProduct = this.cart.find(( item )=>{
+      return item._id == product._id
+    })
+    if (foundProduct){
+      foundProduct.quantity = foundProduct.quantity + 1 //foundProduct.quantity += 1
+      foundProduct.total = foundProduct.price*foundProduct.quantity
+      }else{
+        this.cart.push({
+          _id: product._id,
+          name: product.name,
+          price: product.price,
+          quantity: 1,
+          describe: product.description,
+          urlImage: product.urlImage,
+          total: product.price,
+          stock: product.quantity
+
+        })
+
+    }
+  localStorage.setItem('cart', JSON.stringify(this.cart)) 
+  }
 }
